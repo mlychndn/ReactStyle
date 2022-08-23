c3225ba603b80ca2d061c600cb00151d5292ff67
Malay <mlychndn@gmail.com>
2022-08-24 01:44:24 +0530

    react style implemented

--- src/Components/CousreGoals/CourseInput/CourseInput.js
+++ src/Components/CousreGoals/CourseInput/CourseInput.js
@@ -1,23 +1,32 @@
 import React, { useState } from "react";
-import "./CourseInput.css";
+import styles from "./CourseInput.module.css";
 import Button from "../../UI/Button";
 
 const CourseInput = (props) => {
   const [initialInput, setInitialInput] = useState("");
+  const [isValid, setIsValid] = useState(true);
 
   const inputHandler = (event) => {
+    setIsValid(true);
     setInitialInput(event.target.value);
   };
 
   const courseGoalHandler = (event) => {
     event.preventDefault();
+
+    !initialInput.trim() ? setIsValid(false) : setIsValid(true);
+    if (!initialInput.trim()) {
+      return;
+    }
     props.onCourseAddEvent(initialInput);
     setInitialInput("");
   };
 
   return (
     <form onSubmit={courseGoalHandler}>
-      <div className="form-control">
+      <div
+        className={`${styles["form-control"]} ${!isValid && styles["invalid"]}`}
+      >
         <label>Course Goal</label>
         <input type="text" value={initialInput} onChange={inputHandler} />
       </div>
--- src/Components/UI/Button.js
+++ src/Components/UI/Button.js
@@ -1,9 +1,9 @@
 import React from "react";
-import "./Button.css";
+import styles from "./Button.module.css";
 
 const Button = (props) => {
   return (
-    <button className="button" type={props.type}>
+    <button className={styles.button} type={props.type}>
       {props.children}
     </button>
   );
